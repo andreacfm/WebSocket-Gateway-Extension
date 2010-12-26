78a709dd33b653cfdc3222eb268e97d30f8831fc
Andrea Campolonghi <acampolonghi@gmail.com>
2010-12-26 14:25:05 +0100

    clean project

--- extensions/websocket-gateway-extension/src/railo/extension/gateway/websocket/WebSocketServerImpl.java
+++ extensions/websocket-gateway-extension/src/railo/extension/gateway/websocket/WebSocketServerImpl.java
@@ -1,16 +1,16 @@
 package railo.extension.gateway.websocket;
 
+import net.tootallnate.websocket.WebSocket;
+import net.tootallnate.websocket.WebSocketServer;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
-import net.tootallnate.websocket.WebSocket;
-import net.tootallnate.websocket.WebSocketServer;
-
 public class WebSocketServerImpl extends WebSocketServer {
 	
 	private String _gatewayID;
-	
+
     public WebSocketServerImpl(String port,String id) {
 		super(Integer.parseInt(port));
     	_gatewayID = id;
@@ -45,7 +45,7 @@ public class WebSocketServerImpl extends WebSocketServer {
 
 	/**
 	 *  Sends <var>text</var> to the passed clients if they exists.
-	 * @param connections
+	 * @param conns
 	 * @param text
 	 * @throws IOException
 	 */
